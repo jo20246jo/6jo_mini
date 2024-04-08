33cd3bedc8e80f9ab513cd84a384765e63a581ba
yaejin12 <pos03167@naver.com>
2024-04-08 14:23:59 +0900

    calender 파일명 수정

--- js/calender.js
+++ js/calender.js
@@ -1,8 +1,8 @@
 // 달력 클릭하면 회색 배경으로 변경
-//.click-bg 추가
+//.click-bg 추가하기
 
 document.querySelector("ul.date").addEventListener("click", (e) => {
   const $dayBoxLi = e.target.closest(".day-box");
   console.log(e.target.closest(".day-box"));
-  $dayBoxLi.classList.toggle("click-bg");
+  $dayBoxLi.classList.toggle("click-bg");  
 });
