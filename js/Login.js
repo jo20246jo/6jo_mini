899ffefd0f42f10909f3693230e7f02d662bfcf4
sedica3340 <sedica3340@gmail.com>
2024-04-11 15:24:11 +0900

    로그인정보 로컬스토리지에 저장

--- js/Login.js
+++ js/Login.js
@@ -1,3 +1,12 @@
-document.querySelector('.login-btn').addEventListener("click", () => {
-      alert("로그인에 성공하였습니다!")
-    });
+
+
+document.querySelector(".login-btn").addEventListener("click", () => {
+  const $id = document.querySelector('.user-name');
+  const userName = $id.value;
+  const userNameJSON = JSON.stringify(userName);
+  window.localStorage.setItem("userName", userName);
+    alert(`${userName}님 환영합니다.`);
+});
+document.querySelector('.sub-container').addEventListener('keyup', (e) => {
+  if(e.key === 'Enter') document.querySelector(".login-btn").click();
+})
